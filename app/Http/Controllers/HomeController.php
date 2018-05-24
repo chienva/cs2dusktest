1e1df73f60af21dd3e1a2bff5fb95d341e870400
chienva <chienvuanh@gmail.com>
2018-05-24 12:55:59 +0700

    test mysql laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Users;
+use App\Modules;
+use App\CategoryLarges;
+use DB;
 
 class HomeController extends Controller
 {
+    protected static $_alias = 'talk';
     /**
      * Create a new controller instance.
      *
@@ -26,7 +30,23 @@ class HomeController extends Controller
     {
         // $getUsers = Users::all();
         $getUsers = Users::where('id', 6)->first();
-        return view('home', compact('getUsers'));
+        $getModules = Modules::where('alias', self::$_alias)->first();
+        $getCategoryLarges = CategoryLarges::where('module_id', $getModules->id)->orderBy('sort_order','asc')->get();
+        $talks = DB::table('cs_entry')
+                    ->join('cs_user', 'cs_user.id', '=', 'cs_entry.user_id')
+                    ->select('cs_entry.*', 'cs_user.profile_img', 'cs_user.nickname')
+                    ->whereNull('cs_entry.deleted')
+                    ->whereIn('cs_entry.status', [1, 2])
+                    ->where('cs_entry.module_id', 1)
+                    ->orderBy('cs_entry.num_comment', 'desc')
+                    ->offset(10)
+                    ->limit(10)
+                    ->toSql();
+        $chienva = DB::table('cs_entry')->toSql();
+        dd($talks);
+                    die;
+        // var_dump($getCategoryLarges);die;
+        return view('home', compact('getCategoryLarges'));
     }
 
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,5 @@
 <p>Hello Word</p>
-<p>{{ $getUsers->email }}</p>
+{{--<p>{{ $getCategoryLarges->category_l_alias }}</p>--}}
+@foreach ($getCategoryLarges as $row)
+    <p>This is user {{ $row->title }}</p>
+@endforeach
